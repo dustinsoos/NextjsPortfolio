3aef20e4cfbe03e21761d5b7f1af5e87daf0c5ae
Dustin Soos <106705486+dustinsoos@users.noreply.github.com>
2024-06-10 00:09:45 -0400

    small changes adding prettier sort for tailwind

--- src/app/sections/Introduction/components/IntroContent.tsx
+++ src/app/sections/Introduction/components/IntroContent.tsx
@@ -12,8 +12,8 @@ export default function IntroText() {
       transition={{ duration: 1 }}
       className=' min-h-screen flex justify-center items-center gap-4'
     >
-      <div>
-        <h1 className='text-5xl pb-2'>
+      <div className='w-3/5 flex flex-col gap-4'>
+        <h1 className='text-6xl'>
           Yo, I'm <span className=' text-green-400'>Dustin.</span>
         </h1>
         <p className='text-xl'>
@@ -22,7 +22,7 @@ export default function IntroText() {
           only related to coding but as a person in general.
         </p>
       </div>
-      <div>
+      <div className='w-2/5'>
         <Image src={ProfileImg} alt='Profile-Image' className=' rounded-2xl' />
       </div>
     </motion.div>
