83b8b8928405a2aa4dc7571f7744f873630f1482
Dustin Soos <106705486+dustinsoos@users.noreply.github.com>
2024-07-12 12:38:57 -0400

    some work on my projects section

--- src/app/sections/MyProjects/MyProjects.tsx
+++ src/app/sections/MyProjects/MyProjects.tsx
@@ -11,13 +11,13 @@ export default function MyProjects() {
         className='flex flex-col justify-center items-center md:justify-start md:items-start min-h-screen'
       >
         <h2>Projects</h2>
-        <div className='flex flex-col gap-8 '>
-          <div className='shadow-lg shadow-green-400/30'>
+        <div className='flex flex-col gap-8'>
+          <div className='shadow-lg shadow-green-400/30 flex'>
             <Image src={Portfolio} width={400} height={400} alt='project-img' />
-            <div>
-              <p></p>
-              <p></p>
-              <p></p>
+            <div className='p-4 flex flex-col gap-4'>
+              <h3>My Portfolio</h3>
+              <h4>React | Tailwind CSS | NextJS | Framer-Motion</h4>
+              <p>This is just a brief overview of what I'm about</p>
             </div>
           </div>
           <div className='shadow-lg shadow-green-400/30'>
