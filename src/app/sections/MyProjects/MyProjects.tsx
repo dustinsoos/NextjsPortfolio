4092ee4d1045d0f483f4bf58dcb07f6cc3dc96b7
Dustin Soos <106705486+dustinsoos@users.noreply.github.com>
2024-07-18 21:45:22 -0400

    adding icons/card work

--- src/app/sections/MyProjects/MyProjects.tsx
+++ src/app/sections/MyProjects/MyProjects.tsx
@@ -11,17 +11,20 @@ export default function MyProjects() {
         className='flex flex-col justify-center items-center md:justify-start md:items-start min-h-screen'
       >
         <h2>Projects</h2>
-        <div className='flex flex-col gap-8'>
-          <div className='shadow-lg shadow-green-400/30 flex'>
-            <Image src={Portfolio} width={400} height={400} alt='project-img' />
-            <div className='p-4 flex flex-col gap-4'>
+        <div className='flex flex-col md:flex-row gap-8'>
+          <div className='shadow-lg shadow-green-400/30 flex flex-col max-w-lg'>
+            <Image src={Portfolio} width={520} height={400} alt='project-img' />
+            <div className='p-4 flex flex-col gap-4 text-center'>
               <h3>My Portfolio</h3>
               <h4>React | Tailwind CSS | NextJS | Framer-Motion</h4>
-              <p>This is just a brief overview of what I'm about</p>
+              <p>
+                Just a spot you can check me out where you have my full
+                permission
+              </p>
             </div>
           </div>
           <div className='shadow-lg shadow-green-400/30'>
-            <Image src={faAssets} width={400} height={400} alt='project-img' />
+            <Image src={faAssets} width={520} height={400} alt='project-img' />
           </div>
         </div>
       </section>
