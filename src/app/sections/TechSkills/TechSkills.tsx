bf7f3b5f796c7b14065811a6093618d1d2c659a5
Dustin Soos <106705486+dustinsoos@users.noreply.github.com>
2024-07-19 20:29:52 -0400

    import chakraui
    - added tooltip for icons

--- src/app/sections/TechSkills/TechSkills.tsx
+++ src/app/sections/TechSkills/TechSkills.tsx
@@ -33,54 +33,12 @@ import {
 import '../../styles/iconStyles.css';
 
 export default function TechSkills() {
-  const sections = [
-    {
-      header: 'Frontend',
-      items: [
-        <SiJavascript color='#F7DF1E' className='tech-icon' />,
-        <SiHtml5 color='#E34F26' className='tech-icon' />,
-        <SiCss3 color='#1572B6' className='tech-icon' />,
-        <SiTypescript color='#007ACC' className='tech-icon' />,
-        <SiReact color='#61DAFB' className='tech-icon' />,
-        <SiRedux color='#764ABC' className='tech-icon' />,
-        <SiTailwindcss color='#06B6D4' className='tech-icon' />,
-        <SiBootstrap color='#7952B3' className='tech-icon' />,
-        <SiJquery color='#0769AD' className='tech-icon' />,
-        <SiMui color='#007FFF' className='tech-icon' />,
-        <SiChakraui color='#319795' className='tech-icon' />,
-      ],
-    },
-    {
-      header: 'Backend | Databases',
-      items: [
-        <SiNodedotjs color='#68A063' className='tech-icon' />,
-        <SiPython color='#3776AB' className='tech-icon' />,
-        <SiExpress color='#000000' className='tech-icon bg-white' />,
-        <SiEjs color='#B4CA65' className='tech-icon' />,
-        <SiPostgresql color='#4169E1' className='tech-icon' />,
-      ],
-    },
-    {
-      header: 'Tools',
-      items: [
-        <SiPostman color='#FF6C37' className='tech-icon' />,
-        <SiGithub color='#181717' className='tech-icon bg-white ' />,
-        <SiGit color='#F05032' className='tech-icon' />,
-        <SiNpm color='#CB3837' className='tech-icon' />,
-        <SiVercel color='#000000' className='tech-icon bg-white' />,
-        <SiFigma color='#F24E1E' className='tech-icon' />,
-        <SiJira color='#0052CC' className='tech-icon' />,
-        <SiConfluence className='tech-icon text-blue-500' />,
-      ],
-    },
-  ];
-
   return (
     <>
       <section id='TechSkills' className=' min-h-screen'>
         <h2>TechSkills</h2>
         <div className='flex flex-col justify-start items-start  gap-6'>
-          <TechSkillsList sections={sections} />
+          <TechSkillsList />
         </div>
       </section>
       <hr className='border-t-2 border-green-400 mt-4' />
--- src/app/sections/TechSkills/components/TechSkillsList.tsx
+++ src/app/sections/TechSkills/components/TechSkillsList.tsx
@@ -1,17 +1,146 @@
 import React from 'react';
 import { motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
+import { Tooltip } from '@chakra-ui/react';
 
-interface TechSkillsListProps {
-  sections: {
-    header: string;
-    items: (string | React.ReactNode)[];
-  }[];
-}
+import {
+  SiJavascript,
+  SiHtml5,
+  SiCss3,
+  SiTypescript,
+  SiReact,
+  SiRedux,
+  SiTailwindcss,
+  SiBootstrap,
+  SiJquery,
+  SiVercel,
+  SiGit,
+  SiNodedotjs,
+  SiPython,
+  SiNpm,
+  SiJira,
+  SiConfluence,
+  SiFigma,
+  SiExpress,
+  SiEjs,
+  SiPostgresql,
+  SiMui,
+  SiChakraui,
+  SiPostman,
+  SiGithub,
+} from '@icons-pack/react-simple-icons';
 
-export default function TechSkillsList({ sections }: TechSkillsListProps) {
+export default function TechSkillsList() {
   const controls = useAnimation();
   const { ref, inView } = useInView();
+
+  const sections = [
+    {
+      header: 'Frontend',
+      items: [
+        {
+          name: 'JavaScript',
+          icon: <SiJavascript color='#F7DF1E' className='tech-icon' />,
+        },
+        {
+          name: 'HTML5',
+          icon: <SiHtml5 color='#E34F26' className='tech-icon' />,
+        },
+        {
+          name: 'CSS3',
+          icon: <SiCss3 color='#1572B6' className='tech-icon' />,
+        },
+        {
+          name: 'TypeScript',
+          icon: <SiTypescript color='#007ACC' className='tech-icon' />,
+        },
+        {
+          name: 'React',
+          icon: <SiReact color='#61DAFB' className='tech-icon' />,
+        },
+        {
+          name: 'Redux',
+          icon: <SiRedux color='#764ABC' className='tech-icon' />,
+        },
+        {
+          name: 'Tailwind CSS',
+          icon: <SiTailwindcss color='#06B6D4' className='tech-icon' />,
+        },
+        {
+          name: 'Bootstrap',
+          icon: <SiBootstrap color='#7952B3' className='tech-icon' />,
+        },
+        {
+          name: 'jQuery',
+          icon: <SiJquery color='#0769AD' className='tech-icon' />,
+        },
+        {
+          name: 'Material-UI',
+          icon: <SiMui color='#007FFF' className='tech-icon' />,
+        },
+        {
+          name: 'Chakra UI',
+          icon: <SiChakraui color='#319795' className='tech-icon' />,
+        },
+      ],
+    },
+    {
+      header: 'Backend | Databases',
+      items: [
+        {
+          name: 'Node.js',
+          icon: <SiNodedotjs color='#68A063' className='tech-icon' />,
+        },
+        {
+          name: 'Python',
+          icon: <SiPython color='#3776AB' className='tech-icon' />,
+        },
+        {
+          name: 'Express',
+          icon: <SiExpress color='#000000' className='tech-icon bg-white' />,
+        },
+        {
+          name: 'EJS',
+          icon: <SiEjs color='#B4CA65' className='tech-icon' />,
+        },
+        {
+          name: 'PostgreSQL',
+          icon: <SiPostgresql color='#4169E1' className='tech-icon' />,
+        },
+      ],
+    },
+    {
+      header: 'Tools',
+      items: [
+        {
+          name: 'Postman',
+          icon: <SiPostman color='#FF6C37' className='tech-icon' />,
+        },
+        {
+          name: 'Github',
+          icon: <SiGithub color='#181717' className='tech-icon bg-white ' />,
+        },
+        { name: 'Git', icon: <SiGit color='#F05032' className='tech-icon' /> },
+        { name: 'NPM', icon: <SiNpm color='#CB3837' className='tech-icon' /> },
+        {
+          name: 'Vercel',
+          icon: <SiVercel color='#000000' className='tech-icon bg-white' />,
+        },
+        {
+          name: 'Figma',
+          icon: <SiFigma color='#F24E1E' className='tech-icon' />,
+        },
+        {
+          name: 'Jira',
+          icon: <SiJira color='#0052CC' className='tech-icon' />,
+        },
+        {
+          name: 'Confluence',
+          icon: <SiConfluence className='tech-icon text-blue-500' />,
+        },
+      ],
+    },
+  ];
 
   React.useEffect(() => {
     if (inView) {
@@ -42,7 +171,12 @@ export default function TechSkillsList({ sections }: TechSkillsListProps) {
           <h3 className='uppercase'>{section.header}</h3>
           <ul className='flex flex-wrap gap-2 text-gray-300'>
             {section.items.map((item, itemIndex) => (
-              <li key={itemIndex}>{item}</li>
+              <Tooltip
+                className='text-xs font-bold bg-custom-black text-green-400 px-4 py-1 shadow-lg shadow-gray-300/40 rounded-md'
+                label={item.name}
+              >
+                <li key={itemIndex}>{item.icon}</li>
+              </Tooltip>
             ))}
           </ul>
         </motion.div>
