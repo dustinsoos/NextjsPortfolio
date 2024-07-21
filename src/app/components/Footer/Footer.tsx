d2d9a8df944d9f8a9914fde35a659bd36b8850cb
Dustin Soos <106705486+dustinsoos@users.noreply.github.com>
2024-07-21 14:37:03 -0400

    adding hooks to footer as well

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -2,13 +2,18 @@
 import Link from 'next/link';
 import React from 'react';
 import { motion } from 'framer-motion';
+import useRemoveUrlHash from '@/app/hooks/useRemoveUrlHash';
+import smoothScroll from '@/app/hooks/smoothScroll';
 
 export default function Footer() {
+  useRemoveUrlHash();
+
   return (
     <div className='p-8 flex flex-wrap items-start md:items-center  justify-between pt-32 gap-'>
       <div className='flex flex-col md:flex md:flex-row gap-4'>
         <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
           <Link
+            onClick={(e) => smoothScroll(e, '#Introduction')}
             href='#Introduction'
             className='text-gray-300 uppercase text-sm hover:text-green-400  hover:border-b-2 hover:border-green-400  pb-1'
           >
@@ -17,6 +22,7 @@ export default function Footer() {
         </motion.div>
         <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
           <Link
+            onClick={(e) => smoothScroll(e, '#Experience')}
             href='#Experience'
             className='text-gray-300 uppercase text-sm hover:text-green-400  hover:border-b-2 hover:border-green-400  pb-1'
           >
@@ -25,6 +31,7 @@ export default function Footer() {
         </motion.div>
         <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
           <Link
+            onClick={(e) => smoothScroll(e, '#MyProjects')}
             href='#MyProjects'
             className='text-gray-300 uppercase text-sm hover:text-green-400  hover:border-b-2 hover:border-green-400  pb-1'
           >
@@ -32,15 +39,17 @@ export default function Footer() {
           </Link>
         </motion.div>
         <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-          <a
+          <Link
+            onClick={(e) => smoothScroll(e, '#TechSkills')}
             href='#TechSkills'
             className='text-gray-300 uppercase text-sm hover:text-green-400  hover:border-b-2 hover:border-green-400  pb-1'
           >
             Tech Skills
-          </a>
+          </Link>
         </motion.div>
         <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
           <Link
+            onClick={(e) => smoothScroll(e, '#Contact')}
             href='#Contact'
             className='text-gray-300 uppercase text-sm hover:text-green-400  hover:border-b-2 hover:border-green-400  pb-1'
           >
