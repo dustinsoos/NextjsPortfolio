83ae5b5a537cedd92281a35f2504ec0d6689daf4
Dustin Soos <106705486+dustinsoos@users.noreply.github.com>
2024-07-22 21:14:25 -0400

    resubmitting code test

--- src/app/apis/sendEmail.ts
+++ src/app/apis/sendEmail.ts
@@ -4,6 +4,8 @@ import React from 'react';
 import { Resend } from 'resend';
 import ContactFormEmail from '../../../email/contact-form-email';
 
+import { validateString } from '../../../utils/utils';
+
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
@@ -12,8 +14,20 @@ export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get('email');
   const message = formData.get('message');
 
+  if (!validateString(senderEmail, 500)) {
+    return {
+      error: 'Invalid sender email',
+    };
+  }
+
+  if (!validateString(message, 5000)) {
+    return {
+      error: 'Invalid message',
+    };
+  }
+
   await resend.emails.send({
-    from: 'message@dustinmsoos.com',
+    from: 'contact@dustinmsoos.com',
     to: 'dustinmsoos@gmail.com',
     subject: 'New message from your portfolio!',
     reply_to: senderEmail as string,
