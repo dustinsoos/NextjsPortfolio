5e29a9013453e71d3af236d4f06c742c6b78bbd4
Dustin Soos <106705486+dustinsoos@users.noreply.github.com>
2024-07-24 16:39:47 -0400

    fixing card bug image not fitting container size

--- src/app/sections/MyProjects/MyProjects.tsx
+++ src/app/sections/MyProjects/MyProjects.tsx
@@ -1,22 +1,44 @@
+'use client';
 import Image from 'next/image';
 import React from 'react';
+import Link from 'next/link';
+
+import { motion, useAnimation } from 'framer-motion';
+import { Card, CardBody, Heading, Stack, Text } from '@chakra-ui/react';
+import { SiGithub } from '@icons-pack/react-simple-icons';
+import { useInView } from 'react-intersection-observer';
+
 import linkIcon from '../../../../public/images/link_icon.svg';
 import Portfolio from '../../../../public/images/portfolio.png';
 import faAssets from '../../../../public/images/fa_assets.png';
-import { Card, CardBody, Heading, Stack, Text } from '@chakra-ui/react';
-import Link from 'next/link';
-import { SiGithub } from '@icons-pack/react-simple-icons';
 
 export default function MyProjects() {
+  const controls = useAnimation();
+  const [ref, inView] = useInView({ triggerOnce: true });
+
+  React.useEffect(() => {
+    if (inView) {
+      controls.start({ opacity: 1, scale: 1 });
+    } else {
+      controls.start({ opacity: 0, scale: 0.8 });
+    }
+  }, [controls, inView]);
+
   return (
     <>
       <section
         id='MyProjects'
-        className='flex flex-col justify-center md:justify-start md:items-start min-h-screen pb-40'
+        className='flex  flex-col justify-center md:justify-start md:items-start min-h-screen pb-40'
       >
         <h2>Projects</h2>
-        <div className='flex flex-col md:flex-row gap-8 '>
-          <Card className='flex flex-col gap-4 shadow-lg shadow-green-400/30 w-full md:w-1/2  rounded-lg '>
+        <motion.div
+          ref={ref}
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={controls}
+          transition={{ duration: 1.5 }}
+          className='flex flex-col md:flex-row gap-8 '
+        >
+          <Card className='flex flex-col gap-4 shadow-lg shadow-green-400/30 w-4/5 mx-auto md:mx-0 md:w-1/2  rounded-lg '>
             <CardBody>
               <Image
                 src={Portfolio}
@@ -47,7 +69,7 @@ export default function MyProjects() {
                     />
                   </Link>
                 </div>
-                <h4>
+                <h4 className='pb-4'>
                   React | Tailwind CSS | NextJS | Framer-Motion | ChakraUI |
                   Resend & React Email
                 </h4>
@@ -59,7 +81,7 @@ export default function MyProjects() {
               </Stack>
             </CardBody>
           </Card>
-          <Card className='flex flex-col gap-4 shadow-lg shadow-green-400/30 w-full md:w-1/2  rounded-lg '>
+          <Card className='flex flex-col gap-4 shadow-lg shadow-green-400/30 w-4/5 mx-auto md:mx-0 md:w-1/2  rounded-lg '>
             <CardBody>
               <Image
                 src={faAssets}
@@ -93,7 +115,7 @@ export default function MyProjects() {
                     />
                   </Link>
                 </div>
-                <h4>React | Tailwind CSS | NextJS</h4>
+                <h4 className='pb-4'>React | Tailwind CSS | NextJS</h4>
                 <Text>
                   Frontend Assets was created to help developers find the
                   perfect resources for their projects.
@@ -101,7 +123,7 @@ export default function MyProjects() {
               </Stack>
             </CardBody>
           </Card>
-        </div>
+        </motion.div>
       </section>
       <hr className='border-t-2 border-green-400 mt-4' />
     </>
